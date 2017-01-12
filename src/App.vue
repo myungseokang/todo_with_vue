76f8a309a9397647afa5090f89d1fc87ba0bcfad
Leop0ld <l3opold7@gmail.com>
2017-01-12 17:18:48 +0900

    Add function that writing TODO using vuefire and firebase

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,60 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <hello/>
+    <input
+      v-model.trim="newTodoText"
+      @keyup.enter="addTodo"
+      placeholder="Add new todo">
+    <ul>
+      <li v-for="(todo, index) in todos">
+        <input
+          :value="todo.text"
+          @input="updateTodoText(todo, $event.target.value)">
+        <button @click="removeTodo(todo)">X</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Firebase from 'firebase'
 import Hello from './components/Hello'
-
+// You can copy and paste config code rom the Firebase Console!
+const config = {
+  databaseURL: 'https://leop0ld-3d0a2.firebaseio.com'
+}
+const firebaseApp = Firebase.initializeApp(config)
+const db = firebaseApp.database()
+const todosRef = db.ref('todos')
 export default {
   name: 'app',
+  data () {
+    return {
+      newTodoText: ''
+    }
+  },
   components: {
     Hello
+  },
+  firebase: {
+    todos: todosRef.limitToLast(25)
+  },
+  methods: {
+    addTodo () {
+      if (this.newTodoText) {
+        todosRef.push({
+          text: this.newTodoText
+        })
+        this.newTodoText = ''
+      }
+    },
+    updateTodoText (todo, newText) {
+      todosRef.child(todo['.key']).child('text').set(newText)
+    },
+    removeTodo (todo) {
+      todosRef.child(todo['.key']).remove()
+    }
   }
 }
 </script>
